079cec866a5a72f2d6d073cf95e4718020dc73fe
gaoyx <gaoyx1469@163.com>
2020-07-09 18:06:28 +0800

    mybatis环境搭建

---
+++ src/main/java/top/trial/demo/dao/GameDirectEntityDao.java
@@ -0,0 +1,20 @@
+package top.trial.demo.dao;
+
+import top.trial.demo.entity.GameEntity;
+
+import java.util.List;
+
+/**
+ * @Author gaoyx1469
+ * @Date 2020/7/9
+ * @Version 1.0
+ */
+public interface GameDirectEntityDao {
+
+    /**
+     * 获取所有game
+     * @return List<GameEntity>
+     */
+    List<GameEntity> getAllGames();
+
+}
