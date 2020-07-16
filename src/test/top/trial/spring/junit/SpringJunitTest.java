fc9c1d543ba52c5fe93576fd0ac764c0e5f4310c
gaoyx <gaoyx1469@163.com>
2020-07-16 14:44:34 +0800

    spring整合Junit示例

---
+++ src/test/top/trial/spring/junit/SpringJunitTest.java
@@ -0,0 +1,32 @@
+package top.trial.spring.junit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import top.trial.spring.SpringXml;
+import top.trial.spring.service.AnnotationService;
+
+/**
+ * @Author gaoyx1469
+ * @Date 2020/7/16
+ * @Version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringXml.class)
+//@ContextConfiguration(locations="classpath:bean.xml")
+public class SpringJunitTest {
+
+    @Autowired
+    @Qualifier("allAnnotationService")
+    AnnotationService as;
+
+    @Test
+    public void testAllAnnotationService() {
+        as.getAllGames();
+        as.getGameById(0);
+        as.gameUpdate(null);
+    }
+}
