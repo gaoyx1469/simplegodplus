b2850cfac1f3a1f9f36ec6b3df45f4ab31cd8e4e
gaoyx <gaoyx1469@163.com>
2020-07-18 16:45:02 +0800

    Spring aop通知执行问题

--- src/test/top/trial/spring/aop/SpringAccountOperationAopTest.java
+++ src/test/top/trial/spring/aop/SpringAccountOperationAopTest.java
@@ -14,6 +14,7 @@ import javax.annotation.Resource;
  * @Version 1.0
  */
 @RunWith(SpringJUnit4ClassRunner.class)
+//注意观察两种方式，环绕通知和其它四个通知出现的顺序，以及后置通知和最终通知是否颠倒的情况【跟xml中标签配置顺序有关，详见bean_aop.xml中说明】
 //@ContextConfiguration(locations = "classpath:top/trial/spring/bean_aop.xml")
 @ContextConfiguration(locations = "classpath:top/trial/spring/bean_aop_annotation.xml")
 public class SpringAccountOperationAopTest {
